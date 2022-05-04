78ee86fcfaa1bd39bdc1fb0a491340e906f2e141
snake0818 <101310056+snake0818@users.noreply.github.com>
2022-05-04 14:42:44 +0800

    Update 12-4.cpp

--- 12/12-4/12-4.cpp
+++ 12/12-4/12-4.cpp
@@ -14,7 +14,7 @@ void stuAve(vector<vector<double>>& S1) {
 }
 
 double* courseAve(vector<vector<double>> S1) {
-    double Ave[3];
+    double* Ave = new double[3];
     for (int v2 = 0; v2 < 3; v2++) {
         double sum = 0;
         for (int v1 = 0; v1 < 5; v1++) {
@@ -22,27 +22,26 @@ double* courseAve(vector<vector<double>> S1) {
         }
         Ave[v2] = sum / 5.0;
     }
-    double *p = Ave;
-    
-    return p;
+    return Ave;
 }
 
 void print(vector<vector<double>> S1) {
     for (int v1 = 0; v1 < 5; v1++) {
         for (int v2 = 0; v2 < 4; v2++) {
+            if(v2 > 0) cout << setw (5);
             cout << fixed << setprecision(1) << S1[v1][v2];
-            if (v2 == 3) cout << endl;
-            else cout << ",";
+            if (v2 != 3) cout << ",";
         }
+        cout << endl;
     }
 }
 
 void print(double* Arr, int size = 3) {
     for (int i = 0; i < size; i++) {
         cout << " " << Arr[i];
-        if (i == size - 1) cout << endl;
-        else cout << ",";
+        if (i != size - 1) cout << ",";
     }
+    cout << endl;
 }
 
 int main()
