395b579c3b296703bcb4697fabaf0e9092c5b488
snake0818 <101310056+snake0818@users.noreply.github.com>
2022-05-04 12:10:57 +0800

    修正

--- 12/12-2/12-2.cpp
+++ 12/12-2/12-2.cpp
@@ -13,28 +13,30 @@ int main()
     }
     for (int i = 0; i < 4; i++) {
         int sum = 0;
-        intArr[0] = cs.pop()-48;
+        intArr[0] = cs.pop() - 48;
         ch = cs.pop();
-        intArr[1] = cs.pop()-48;
+        intArr[1] = cs.pop() - 48;
 
         if (ch == '+') {
             sum = intArr[1] + intArr[0];
+            cout << sum << endl;
         }
         else if (ch == '-') {
             sum = intArr[1] - intArr[0];
+            cout << sum << endl;
         }
         else if (ch == '*') {
-            sum = intArr[1] * intArr[0];            
+            sum = intArr[1] * intArr[0];
+            cout << sum << endl;
         }
         else if (ch == '/') {
-            sum = intArr[1] / intArr[0];            
+            sum = intArr[1] / intArr[0];
+            cout << sum << endl;
         }
-        ch = sum+'0';
+        ch = sum + '0';
         cs.push(ch);
 
     }
-
-    cs.printStack(cs);
 
     return 0;
 }
