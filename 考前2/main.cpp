12eb7014889d45edc5cc2efed0790d7826128b49
snake0818 <101310056+snake0818@users.noreply.github.com>
2022-06-08 13:58:05 +0800

    Update main.cpp

--- 考前2/main.cpp
+++ 考前2/main.cpp
@@ -13,14 +13,12 @@ void display(Shape& s)
 	cout << fixed << setprecision(0);
 	if (p1 != NULL)
 	{
-		cout << p1->toString() << ":";
-		cout << "(" << p1->getRadius() << "),"
+		cout << p1->toString()<< ":(" << p1->getRadius() << "),"
 			<< fixed << setprecision(1) << p1->getVolume() << ",";
 	}
 	if (p2 != NULL)
 	{
-		cout << p2->toString() << ":";
-		cout << "("
+		cout << p2->toString()<< ";("
 			<< p2->getLength() << "," << p2->getWidth() << "," << p2->getHeight() << "),"
 			<< fixed << setprecision(1) << p2->getVolume() << ",";
 	}
